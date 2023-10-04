0467283b579d55d09308ed43f6a89f7610c47a04
GareFunkle <vincent.durret@gmail.com>
2023-10-04 20:30:41 +0200

    //

--- src/views/Home/Section/OurVision/OurVision.jsx
+++ src/views/Home/Section/OurVision/OurVision.jsx
@@ -3,5 +3,36 @@ import React from "react";
 import "./OurVision.scss";
 
 const OurVision = () => {
-  return <section className="ourvision"></section>;
+  return (
+    <section className="ourvision">
+      <div className="ourvision__container">
+        <h2>Notre vision</h2>
+        <p>
+          Dans un monde en constante évolution, le digital est devenu bien plus
+          qu'un simple outil : il est le terrain sur lequel les rêves prennent
+          forme et les idées s'épanouissent.
+        </p>
+        <p>
+          Chez GIVI notre vision est claire : vous guider et vous accompagner
+          dans cet univers.
+        </p>
+        <p>
+          Notre objectif principal est de vous aider à naviguer avec confiance
+          dans ce monde numérique, de transformer vos ambitions en réalités
+          tangibles et d'assurer que votre voix soit entendue dans cette vaste
+          mer d'innovations.
+        </p>
+        <p>
+          Ensemble, explorons les opportunités du monde 2.0 et construisons
+          votre succès.
+        </p>
+      </div>
+      <div className="ourvision__img">
+        <img src="" alt="" />
+      </div>
+      <div className="ouvision__actions">
+        <button>en savoir plus </button>
+      </div>
+    </section>
+  );
 };
